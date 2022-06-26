f1acfb9ac544c039f3ce33b35ba24f6372c25569
umang  talpra <patelumang607@gmail.com>
2022-06-26 13:16:17 +0530

    contact  page done

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -13,14 +13,14 @@
     <div class="map_wrap">
       <iframe
         id="gmap_canvas"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d705.2314238733026!2d72.52986612914003!3d23.02433639905953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84cf948b7bb5%3A0xd565a1844fb10171!2sShivranjani%20BRTS!5e1!3m2!1sen!2sin!4v1656093410837!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2820.902326880377!2d72.52927871416654!3d23.0254532349516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84cf75f23e8d%3A0x1fb23b359dce1155!2sParishram%20Apartment!5e1!3m2!1sen!2sin!4v1656229477505!5m2!1sen!2sin"
         frameborder="0"
         scrolling="no"
         marginheight="0"
         marginwidth="0"
       ></iframe>
     </div>
-    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d705.2314238733026!2d72.52986612914003!3d23.02433639905953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84cf948b7bb5%3A0xd565a1844fb10171!2sShivranjani%20BRTS!5e1!3m2!1sen!2sin!4v1656093410837!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2820.902326880377!2d72.52927871416654!3d23.0254532349516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84cf75f23e8d%3A0x1fb23b359dce1155!2sParishram%20Apartment!5e1!3m2!1sen!2sin!4v1656229477505!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
     <!-- End Google Map -->
 
     <!-- <div class="tokyo_tm_about">
